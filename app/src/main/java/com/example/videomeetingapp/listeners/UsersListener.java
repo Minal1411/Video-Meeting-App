e72fd477558ecab9e3d0bea6aed59bd5a05d9d84
Min Chhetri <min.chhetri@helpincpvt.com>
2020-10-08 09:08:24 +0545

    code is added

---
+++ app/src/main/java/com/example/videomeetingapp/listeners/UsersListener.java
@@ -0,0 +1,9 @@
+package com.example.videomeetingapp.listeners;
+
+import com.example.videomeetingapp.models.User;
+
+public interface UsersListener {
+    void initiateVideoMeeting(User user);
+    void initiateAudioMeeting(User user);
+    //void onMultipleUsersAction(Boolean isMultipleUsersSelected);
+}
---
+++ app/src/main/java/com/example/videomeetingapp/utilities/Constants.java
@@ -0,0 +1,38 @@
+package com.example.videomeetingapp.utilities;
+
+import java.util.HashMap;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_FIRST_NAME = "first_name";
+    public static final String KEY_LAST_NAME = "last_name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "videoMeetingPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_USER_ID = "user_id";
+    public static final String KEY_FCM_TOKEN = "fcm_token";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_TYPE = "type";
+    public static final String REMOTE_MSG_INVITATION = "invitation";
+    public static final String REMOTE_MSG_MEETING_TYPE = "meetingType";
+    public static final String REMOTE_MSG_INVITER_TOKEN = "inviterToken";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+    public static final String REMOTE_MSG_INVITATION_RESPONSE = "invitationResponse";
+    public static final String REMOTE_MSG_INVITATION_ACCEPTED = "accepted";
+    public static final String REMOTE_MSG_INVITATION_REJECTED = "rejected";
+    public static final String REMOTE_MSG_INVITATION_CANCELLED = "cancelled";
+    public static final String REMOTE_MSG_MEETING_ROOM = "meetingRoom";
+
+    public static HashMap<String, String> getRemoteMessageHeaders() {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put(
+                Constants.REMOTE_MSG_AUTHORIZATION,
+                "key=AAAA1yKDXIs:APA91bGiUEdYeTMyppasOvh4scldfDqEw8a3_IV5G3ZRsAzyPNh9p0Khk2lE32AVqaB7LISIXa_CoBeNb_tU1X43BwF69PejTPiNDEvvyKgxbfRM0zCYF3Ta-J_pg5ZpXtzzy8sT29A8"
+        );
+        headers.put(Constants.REMOTE_MSG_CONTENT_TYPE, "application/json");
+        return headers;
+    }
+}
